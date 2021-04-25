6c4e290fdd90897ec6050c0de93267ac43d686cf
Maryam-Ejaz <mariam.ejaz12@gmail.com>
2021-04-25 06:47:19 +0500

    2 string manipulation functions

---
+++ Task4/src/Task_4/MyString.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Task_4;
+
+/**
+ * 
+ * @author Maryam Ejaz
+ */
+public class MyString{
+    
+    public String inputString;
+   
+    public MyString(String inputString){
+        this.inputString = inputString;
+    }
+    
+    /*
+     * This function removes spaces from the string
+     * @returns String
+     */
+    public String removeSpaces(){
+        String StrwithoutSpaces;
+        //Returns size exluding spaces. 
+        int size = this.inputString.length()- this.numberOfSpaces();
+
+        char[] array = new char[size];
+        
+        //Exclusion of spaces from a string
+        for(int y=0, x=0; y<this.inputString.length(); y++){
+            if (this.inputString.charAt(y) == ' '){
+                //ignores space
+            }
+            else{
+                array[x] = this.inputString.charAt(y);
+                x++;
+            }
+        }
+        
+        StrwithoutSpaces = String.copyValueOf(array);
+        
+        return StrwithoutSpaces;
+    }
+    
+    /*
+     * This function returns number of spaces in a string.
+     * @returns int
+     */
+    public int numberOfSpaces(){
+        int space = 0;
+        for(int i=0; i<this.inputString.length(); i++){
+            if (this.inputString.charAt(i) == ' '){
+                space++;
+            }
+        }
+        return space;
+    }
+    
+}
